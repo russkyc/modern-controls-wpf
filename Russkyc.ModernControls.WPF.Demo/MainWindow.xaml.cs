666b32d3ce1fc313d849b966fd1bb3fa840fd900
russkyc <jrcc1023@gmail.com>
2023-04-02 18:28:08 +0800

    chore: update demo project and screenshot

--- Russkyc.ModernControls.WPF.Demo/MainWindow.xaml.cs
+++ Russkyc.ModernControls.WPF.Demo/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Russkyc.ModernControls.WPF.Demo
 {
@@ -20,11 +7,6 @@ namespace Russkyc.ModernControls.WPF.Demo
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            
-            for (int i = 1; i <= 20; i++) CmbPrimary.Items.Add(i);
-        }
+        
     }
 }
---
+++ Russkyc.ModernControls.WPF.Demo/RegistrationViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Russkyc.ModernControls.WPF.Demo;
+
+partial class RegistrationViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _firstName;
+    
+    [ObservableProperty]
+    private string _middleName;
+    
+    [ObservableProperty]
+    private string _lastName;
+    
+    [ObservableProperty]
+    private bool _male;
+    
+    [ObservableProperty]
+    private bool _female;
+
+    [ObservableProperty]
+    private ObservableCollection<int> _day;
+    
+    [ObservableProperty]
+    private ObservableCollection<int> _month;
+    
+    [ObservableProperty]
+    private ObservableCollection<int> _year;
+
+    [ObservableProperty]
+    private int _selectedDay;
+    
+    [ObservableProperty]
+    private int _selectedMonth;
+    
+    [ObservableProperty]
+    private int _selectedYear;
+
+    public RegistrationViewModel()
+    {
+
+        SelectedDay = 0;
+        SelectedMonth = 0;
+        SelectedYear = 0;
+
+        Day = new ObservableCollection<int>();
+        Month = new ObservableCollection<int>();
+        Year = new ObservableCollection<int>();
+            
+        for (int i = 1; i <= 30; i++) Day.Add(i);
+        for (int i = 1; i <= 12; i++) Month.Add(i);
+        for (int i = 1900; i <= 2022; i++) Year.Add(i);
+    }
+
+    [RelayCommand]
+    void Register()
+    {
+        var name = $"{FirstName} {MiddleName} {LastName}";
+        var gender = Male ? "Male" : Female ? "Female" : "Prefer Not to Say";
+        var dateOfBirth = $"{Day[SelectedDay]}/{Month[SelectedMonth]}/{Year[SelectedYear]}";
+        MessageBox.Show($"StudentName: {name} {Environment.NewLine}Gender: {gender}{Environment.NewLine}Date of Birth: {dateOfBirth}");
+    }
+}
