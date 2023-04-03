3b5d8efa38df0d16bbf0b9ed911bc189feae4f8b
russkyc <jrcc1023@gmail.com>
2023-04-04 01:25:58 +0800

    chore: update demo to showcase new api

--- Russkyc.ModernControls.WPF.Demo/RegistrationViewModel.cs
+++ Russkyc.ModernControls.WPF.Demo/RegistrationViewModel.cs
@@ -20,30 +20,21 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System;
 using System.Collections.ObjectModel;
-using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 namespace Russkyc.ModernControls.WPF.Demo;
 
-partial class RegistrationViewModel : ObservableObject
+public partial class RegistrationViewModel : ObservableObject
 {
     [ObservableProperty]
-    private string _firstName;
+    private string _email;
     
     [ObservableProperty]
-    private string _middleName;
+    private string _studentNumber;
     
     [ObservableProperty]
-    private string _lastName;
-    
-    [ObservableProperty]
-    private bool _male;
-    
-    [ObservableProperty]
-    private bool _female;
+    private string _name;
 
     [ObservableProperty]
     private ObservableCollection<int> _day;
@@ -54,21 +45,8 @@ partial class RegistrationViewModel : ObservableObject
     [ObservableProperty]
     private ObservableCollection<int> _year;
 
-    [ObservableProperty]
-    private int _selectedDay;
-    
-    [ObservableProperty]
-    private int _selectedMonth;
-    
-    [ObservableProperty]
-    private int _selectedYear;
-
     public RegistrationViewModel()
     {
-
-        SelectedDay = 0;
-        SelectedMonth = 0;
-        SelectedYear = 0;
 
         Day = new ObservableCollection<int>();
         Month = new ObservableCollection<int>();
@@ -77,14 +55,5 @@ partial class RegistrationViewModel : ObservableObject
         for (int i = 1; i <= 30; i++) Day.Add(i);
         for (int i = 1; i <= 12; i++) Month.Add(i);
         for (int i = 1900; i <= 2022; i++) Year.Add(i);
-    }
-
-    [RelayCommand]
-    void Register()
-    {
-        var name = $"{FirstName} {MiddleName} {LastName}";
-        var gender = Male ? "Male" : Female ? "Female" : "Prefer Not to Say";
-        var dateOfBirth = $"{Day[SelectedDay]}/{Month[SelectedMonth]}/{Year[SelectedYear]}";
-        MessageBox.Show($"StudentName: {name} {Environment.NewLine}Gender: {gender}{Environment.NewLine}Date of Birth: {dateOfBirth}");
     }
 }
