352d657a543c2712276904469f1606c6c0b16843
russkyc <jrcc1023@gmail.com>
2023-04-08 16:00:39 +0800

    chore: update theme implementation in demo application

--- Russkyc.ModernControls.WPF.Demo/App.xaml.cs
+++ Russkyc.ModernControls.WPF.Demo/App.xaml.cs
@@ -37,8 +37,8 @@ namespace Russkyc.ModernControls.WPF.Demo
             base.OnStartup(e);
             
             // Add Themes
+            ThemeHelper.AddTheme("Night","pack://application:,,,/Russkyc.ModernControls.WPF;component/Themes/ColorThemes/DefaultDark.xaml");
             ThemeHelper.AddTheme("Day","pack://application:,,,/Russkyc.ModernControls.WPF;component/Themes/ColorThemes/DefaultLight.xaml");
-            ThemeHelper.AddTheme("Night","pack://application:,,,/Russkyc.ModernControls.WPF;component/Themes/ColorThemes/RazerDark.xaml");
-        }
+            }
     }
 }
